2dffbc2e219d96af879fccdf9f112ff85ed26651
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-29 22:49:38 +0200

    - created truncate pipe

--- src/app/movie-grid/movie-grid.module.ts
+++ src/app/movie-grid/movie-grid.module.ts
@@ -6,11 +6,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MovieGridComponent } from './movie-grid.component';
 import { MovieCardComponent } from './shared/movie-card/movie-card.component';
 import { MovieApiService } from './services/movie-api.service';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 
 
 @NgModule({
-  declarations: [MovieGridComponent, MovieCardComponent],
+  declarations: [MovieGridComponent, MovieCardComponent, TruncatePipe],
   imports: [
     CommonModule,
     HttpClientModule,
