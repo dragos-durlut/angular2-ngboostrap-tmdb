eeb21d704b06d38932b3956dbab0ee2e9b1b1d38
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-29 22:08:26 +0200

    - bind movie object to movie-card

--- src/app/movie-grid/shared/movie-card/movie-card.component.ts
+++ src/app/movie-grid/shared/movie-card/movie-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: '[app-movie-card]',
@@ -7,15 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieCardComponent implements OnInit {
 
-  public randomNumber: number;
+  @Input() movie: Movie;
+
+  private imageBaseUrl: string = "https://image.tmdb.org/t/p/w500";
+
+  public posterImageUrl: string;
+
+  
 
   constructor() {
     this.randomNumber = this.randomIntFromInterval(100, 1000);
   }
 
   ngOnInit(): void {
+    this.posterImageUrl = `${this.imageBaseUrl}${this.movie.poster_path}`;
   }
 
+
+  public randomNumber: number;
   private randomIntFromInterval(min, max): number { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
