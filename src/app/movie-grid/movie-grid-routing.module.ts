2efc5ae8a881d2643f424e6a025ccba6bf89eacf
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-29 19:39:04 +0200

    - added movie-grid component route

--- src/app/movie-grid/movie-grid-routing.module.ts
+++ src/app/movie-grid/movie-grid-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieGridComponent } from './movie-grid.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movie-grid'
+    , component: MovieGridComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
