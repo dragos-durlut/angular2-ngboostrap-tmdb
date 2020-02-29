aa4e27b134085c1c9c96f21bbf13475ff1045fbb
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-29 22:27:12 +0200

    - removed random number generator

--- src/app/movie-grid/shared/movie-card/movie-card.component.ts
+++ src/app/movie-grid/shared/movie-card/movie-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: '[app-movie-card]',
@@ -16,16 +17,10 @@ export class MovieCardComponent implements OnInit {
   
 
   constructor() {
-    this.randomNumber = this.randomIntFromInterval(100, 1000);
+    
   }
 
   ngOnInit(): void {
     this.posterImageUrl = `${this.imageBaseUrl}${this.movie.poster_path}`;
   }
-
-
-  public randomNumber: number;
-  private randomIntFromInterval(min, max): number { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 }
