ea462c8acbc39e7cdc4913de6c5be3f7cb9b4191
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-29 18:08:46 +0200

    - created movie-grid module with movie-grid component

---
+++ src/app/movie-grid/movie-grid.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MovieGridRoutingModule } from './movie-grid-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MovieGridComponent } from './movie-grid.component';
+
+
+@NgModule({
+  declarations: [MovieGridComponent],
+  imports: [
+    CommonModule,
+    MovieGridRoutingModule,
+    NgbModule
+  ]
+})
+export class MovieGridModule { }
