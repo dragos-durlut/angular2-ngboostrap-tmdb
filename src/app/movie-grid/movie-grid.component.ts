a3e9fdf2124487969dcbeb92c273ae37a3d5da87
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-03-02 13:50:01 +0200

    - added paging

--- src/app/movie-grid/movie-grid.component.ts
+++ src/app/movie-grid/movie-grid.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieApiService } from './services/movie-api.service';
+import { Observable, of, Subject } from 'rxjs';
+import { NgbPaginationConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-movie-grid',
   templateUrl: './movie-grid.component.html',
-  styleUrls: ['./movie-grid.component.scss']
+  styleUrls: ['./movie-grid.component.scss'],
+  providers: [NgbPaginationConfig] 
 })
 export class MovieGridComponent implements OnInit {
 
+  private _currentPage = 1;
+  
+  public get currentPage() {
+    return this._currentPage;
+  }
+  public set currentPage(page: number) {
+    this._currentPage = page;
+    this.getMostPopularMovies(page);
+  }
+
+  public totalNumberOfResults: number;
+  private totalNumberOfPages: number;
+
+
+  public nrOfPagesToDisplay = 5;
+  public pageSize = 20;
+
   public movies: Movie[] = new Array<Movie>();
-  public moviesPage: MoviePage;
 
-
-  constructor(private movieApiService: MovieApiService) {
+  constructor(private movieApiService: MovieApiService, config: NgbPaginationConfig) {
+    config.size = 'lg';
   }
 
   ngOnInit() {
-    this.movieApiService.getTheMostPopularMovies().subscribe(moviesPage => { this.moviesPage = moviesPage; this.movies = moviesPage.results });
+    this.getMostPopularMovies(1);
+  }
+
+  private getMostPopularMovies(page: number): void {
+    this.movieApiService.getTheMostPopularMovies(page).subscribe(moviesPage => {
+      this.movies = moviesPage.results;
+      this._currentPage = moviesPage.page;
+      this.totalNumberOfPages = moviesPage.total_pages;
+      this.totalNumberOfResults = moviesPage.total_results;
+    });
   }
 
 }
