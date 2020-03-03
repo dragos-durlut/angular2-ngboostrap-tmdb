13c1bc201b1fc3ab543c6148adc57912a87ce09a
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-03-03 12:33:11 +0200

    - added getGenres() func

--- src/app/movie-grid/services/movie-api.service.ts
+++ src/app/movie-grid/services/movie-api.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { MoviePage } from '../interfaces/movie-page';
 import { MovieFilter } from '../interfaces/movie-filter';
 import { Certification } from '../interfaces/certification';
+import { Genre } from '../interfaces/genre';
 
 @Injectable({
   providedIn: 'root'
@@ -74,5 +75,87 @@ export class MovieApiService {
         "order": 5
       }];
     return certificationsArray;
+  }
+
+  public getGenres(): Array<Genre> {
+    let genresArray: Array<Genre> = <Array<Genre>>
+      [
+        {
+          "id": 28,
+          "name": "Action"
+        },
+        {
+          "id": 12,
+          "name": "Adventure"
+        },
+        {
+          "id": 16,
+          "name": "Animation"
+        },
+        {
+          "id": 35,
+          "name": "Comedy"
+        },
+        {
+          "id": 80,
+          "name": "Crime"
+        },
+        {
+          "id": 99,
+          "name": "Documentary"
+        },
+        {
+          "id": 18,
+          "name": "Drama"
+        },
+        {
+          "id": 10751,
+          "name": "Family"
+        },
+        {
+          "id": 14,
+          "name": "Fantasy"
+        },
+        {
+          "id": 36,
+          "name": "History"
+        },
+        {
+          "id": 27,
+          "name": "Horror"
+        },
+        {
+          "id": 10402,
+          "name": "Music"
+        },
+        {
+          "id": 9648,
+          "name": "Mystery"
+        },
+        {
+          "id": 10749,
+          "name": "Romance"
+        },
+        {
+          "id": 878,
+          "name": "Science Fiction"
+        },
+        {
+          "id": 10770,
+          "name": "TV Movie"
+        },
+        {
+          "id": 53,
+          "name": "Thriller"
+        },
+        {
+          "id": 10752,
+          "name": "War"
+        },
+        {
+          "id": 37,
+          "name": "Western"
+        }];
+    return genresArray;
   }
 }
